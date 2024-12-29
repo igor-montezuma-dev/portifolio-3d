f991b5b8f8fe1b0cc14d87380c789504d1ea038f
IgorMontezuma20 <igorminranda17@gmail.com>
2024-12-28 23:55:58 -0300

    feat: Create navbar component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "./sections/navbar.jsx";
+
+const App = () => {
+    return (
+        <main className="max-w-7xl mx-auto">
+            <Navbar />
+        </main>
+    )
+}
+export default App
