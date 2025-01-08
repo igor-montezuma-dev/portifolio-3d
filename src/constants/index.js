9ae6e8a369a423e94d6ab526ba20d4997550b7ae
IgorMontezuma20 <igorminranda17@gmail.com>
2025-01-08 10:32:27 -0300

    feat: Add coop dev project in projects section component

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,238 +1,287 @@
 export const navLinks = [
-    {
-        id: 1,
-        name: 'Home',
-        href: '#home',
-    },
-    {
-        id: 2,
-        name: 'Sobre',
-        href: '#about',
-    },
-    {
-        id: 3,
-        name: 'Trabalhos',
-        href: '#work',
-    },
-    {
-        id: 4,
-        name: 'Experiência',
-        href: '#experience',
-    },
-    {
-        id: 5,
-        name: 'Minhas redes',
-        href: '#socials',
-    }
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "Sobre",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Trabalhos",
+    href: "#work",
+  },
+  {
+    id: 4,
+    name: "Experiência",
+    href: "#experience",
+  },
+  {
+    id: 5,
+    name: "Minhas redes",
+    href: "#socials",
+  },
 ];
 
-
-
 export const myProjects = [
-    {
-        title: 'Chat.Io - Aplicação de chat em tempo real',
-        desc: 'Este projeto foi desenvolvido como parte de um estudo aprofundado sobre aplicações em tempo real utilizando Socket.io. Trata-se de uma aplicação full stack construída com React.js no frontend e Node.js no backend, projetada para oferecer uma experiência fluida e interativa para seus usuários.',
-        subdesc:
-            'Este projeto é um exemplo prático e funcional da aplicação de conceitos modernos de desenvolvimento web, com foco em tecnologias em tempo real e usabilidade.',
-        href: 'https://chat-io-vzqv.onrender.com/login',
-        texture: '/textures/project/chat-io.png',
-        logo: '/assets/chat-io-logo.png',
-        logoStyle: {
-            backgroundColor: '#3D3244',
-            border: '0.2px solid #36201D',
-            boxShadow: '0px 0px 60px 0px #AA3C304D',
-        },
-        spotlight: '/assets/spotlight1.png',
-        tags: [
-            {
-                id: 1,
-                name: 'React.js',
-                path: '/assets/react.svg',
-            },
-            {
-                id: 2,
-                name: 'TailwindCSS',
-                path: 'assets/tailwindcss.png',
-            },
-            {
-                id: 3,
-                name: 'TypeScript',
-                path: '/assets/typescript.png',
-            },
-            {
-                id: 4,
-                name: 'Node.js',
-                path: '/assets/node.png',
-            },
-            {
-                id: 5,
-                name: 'Socket.io',
-                path: '/assets/socket.png',
-            }
-        ],
+  {
+    title: "Chat.Io - Aplicação de chat em tempo real",
+    desc: "Este projeto foi desenvolvido como parte de um estudo aprofundado sobre aplicações em tempo real utilizando Socket.io. Trata-se de uma aplicação full stack construída com React.js no frontend e Node.js no backend, projetada para oferecer uma experiência fluida e interativa para seus usuários.",
+    subdesc:
+      "Este projeto é um exemplo prático e funcional da aplicação de conceitos modernos de desenvolvimento web, com foco em tecnologias em tempo real e usabilidade.",
+    href: "https://chat-io-vzqv.onrender.com/login",
+    texture: "/textures/project/chat-io.png",
+    logo: "/assets/chat-io-logo.png",
+    logoStyle: {
+      backgroundColor: "#3D3244",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
     },
-    {
-        title: 'NgFlix - Confira os últimos lançamentos',
-        desc: 'Este projeto foi criado com o objetivo de explorar as potencialidades do Angular, destacando sua versatilidade como framework completo para desenvolvimento web. A aplicação oferece aos usuários uma experiência intuitiva e prática para acessar informações detalhadas sobre filmes em cartaz.',
-        subdesc:
-            'Este projeto reafirma a flexibilidade do Angular, mostrando como ele pode transformar o ambiente de desenvolvimento com ferramentas poderosas e uma abordagem reativa e eficiente.',
-        href: 'https://angular-flix.vercel.app',
-        texture: '/textures/project/ng-flix.png',
-        logo: '/assets/netflix-logo.png',
-        logoStyle: {
-            backgroundColor: '#13202F',
-            border: '0.2px solid #17293E',
-            boxShadow: '0px 0px 60px 0px #2F6DB54D',
-        },
-        spotlight: '/assets/spotlight2.png',
-        tags: [
-            {
-                id: 1,
-                name: 'Angular',
-                path: '/assets/angular-logo.png',
-            },
-            {
-                id: 2,
-                name: 'Sass',
-                path: 'assets/sass-logo.png',
-            },
-            {
-                id: 3,
-                name: 'TypeScript',
-                path: '/assets/typescript.png',
-            },
-            {
-                id: 4,
-                name: 'Rxjs',
-                path: '/assets/rxjs-logo.png',
-            },
-        ],
+    spotlight: "/assets/spotlight1.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Node.js",
+        path: "/assets/node.png",
+      },
+      {
+        id: 5,
+        name: "Socket.io",
+        path: "/assets/socket.png",
+      },
+    ],
+  },
+  {
+    title: "NgFlix - Confira os últimos lançamentos",
+    desc: "Este projeto foi criado com o objetivo de explorar as potencialidades do Angular, destacando sua versatilidade como framework completo para desenvolvimento web. A aplicação oferece aos usuários uma experiência intuitiva e prática para acessar informações detalhadas sobre filmes em cartaz.",
+    subdesc:
+      "Este projeto reafirma a flexibilidade do Angular, mostrando como ele pode transformar o ambiente de desenvolvimento com ferramentas poderosas e uma abordagem reativa e eficiente.",
+    href: "https://angular-flix.vercel.app",
+    texture: "/textures/project/ng-flix.png",
+    logo: "/assets/netflix-logo.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
     },
-    {
-        title: 'Help Control - Plataforma Help Desk',
-        desc: 'Este projeto foi desenvolvido para explorar e aplicar os novos recursos do Next.js 14 em uma plataforma Help Desk Full Stack. A aplicação combina funcionalidades modernas de front e backend para oferecer uma experiência fluida e eficiente aos usuários.',
-        subdesc:
-            'O projeto demonstra a versatilidade e o poder do Next.js 14, reforçando seu valor no mercado como uma ferramenta completa para o desenvolvimento de aplicações modernas e robustas.',
-        href: 'https://help-control.vercel.app',
-        texture: '/textures/project/help-control.png',
-        logo: '/assets/computer-icon.png',
-        logoStyle: {
-            backgroundColor: '#60f5a1',
-            background:
-                'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
-            border: '0.2px solid rgba(208, 213, 221, 1)',
-            boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
-        },
-        spotlight: '/assets/spotlight3.png',
-        tags: [
-            {
-                id: 1,
-                name: 'Next.js',
-                path: '/assets/nextjs.png',
-            },
-            {
-                id: 2,
-                name: 'TailwindCSS',
-                path: 'assets/tailwindcss.png',
-            },
-            {
-                id: 3,
-                name: 'TypeScript',
-                path: '/assets/typescript.png',
-            },
-            {
-                id: 4,
-                name: 'MongoDB',
-                path: '/assets/mongo-db.png',
-            },
-        ],
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "Angular",
+        path: "/assets/angular-logo.png",
+      },
+      {
+        id: 2,
+        name: "Sass",
+        path: "assets/sass-logo.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Rxjs",
+        path: "/assets/rxjs-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Help Control - Plataforma Help Desk",
+    desc: "Este projeto foi desenvolvido para explorar e aplicar os novos recursos do Next.js 14 em uma plataforma Help Desk Full Stack. A aplicação combina funcionalidades modernas de front e backend para oferecer uma experiência fluida e eficiente aos usuários.",
+    subdesc:
+      "O projeto demonstra a versatilidade e o poder do Next.js 14, reforçando seu valor no mercado como uma ferramenta completa para o desenvolvimento de aplicações modernas e robustas.",
+    href: "https://help-control.vercel.app",
+    texture: "/textures/project/help-control.png",
+    logo: "/assets/computer-icon.png",
+    logoStyle: {
+      backgroundColor: "#60f5a1",
+      background:
+        "linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)",
+      border: "0.2px solid rgba(208, 213, 221, 1)",
+      boxShadow: "0px 0px 60px 0px rgba(35, 131, 96, 0.3)",
     },
-    {
-        title: 'Em breve',
-        desc: '...',
-        subdesc:
-            '...',
-        href: 'https://www.linkedin.com/in/igor-montezuma-dev/',
-        texture: '/textures/project/angular-logo.png',
-        logo: '/assets/angular-logo.png',
-        logoStyle: {
-            backgroundColor: '#0E1F38',
-            border: '0.2px solid #0E2D58',
-            boxShadow: '0px 0px 60px 0px #2F67B64D',
-        },
-        spotlight: '/assets/spotlight4.png',
-        tags: [],
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/nextjs.png",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "MongoDB",
+        path: "/assets/mongo-db.png",
+      },
+    ],
+  },
+  {
+    title: "Coop Dev",
+    desc: "Este site foi desenvolvido para a Coop Dev, uma cooperativa de desenvolvedores que tem como objetivo promover a colaboração e o compartilhamento de conhecimento entre profissionais da área de tecnologia. O projeto foi construído com Angular e TailwindCSS, e destaca a importância da comunidade e do trabalho em equipe.",
+    subdesc:
+      "O site da Coop Dev é um exemplo prático de como a tecnologia pode ser usada para unir pessoas e promover a troca de experiências e ideias.",
+    href: "https://coopdev.com.br/",
+    texture: "/textures/project/coop-dev.png",
+    logo: "/assets/coop-dev.png",
+    logoStyle: {
+      backgroundColor: "#0E1F38",
+      border: "0.2px solid #0E2D58",
+      boxShadow: "0px 0px 60px 0px #2F67B64D",
     },
-    {
-        title: 'Em breve',
-        desc: '...',
-        subdesc:
-            '...',
-        href: 'https://www.linkedin.com/in/igor-montezuma-dev/',
-        texture: '/textures/project/logo-react.png',
-        logo: '/assets/logo-react.png',
-        logoStyle: {
-            backgroundColor: '#1C1A43',
-            border: '0.2px solid #252262',
-            boxShadow: '0px 0px 60px 0px #635BFF4D',
-        },
-        spotlight: '/assets/spotlight5.png',
-        tags: [],
+    spotlight: "/assets/spotlight4.png",
+    tags: [
+      {
+        id: 1,
+        name: "Angular",
+        path: "/assets/angular-logo.png",
+      },
+      {
+        id: 2,
+        name: "Sass",
+        path: "assets/sass-logo.png",
+      },
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 4,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 5,
+        name: "Rxjs",
+        path: "/assets/rxjs-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Em breve",
+    desc: "...",
+    subdesc: "...",
+    href: "https://www.linkedin.com/in/igor-montezuma-dev/",
+    texture: "/textures/project/logo-react.png",
+    logo: "/assets/logo-react.png",
+    logoStyle: {
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
     },
+    spotlight: "/assets/spotlight5.png",
+    tags: [],
+  },
 ];
 
 export const calculateSizes = (isSmall, isMobile, isTablet) => {
-    return {
-        deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
-        deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -6, 0],
-        cubePosition: isSmall ? [4, -5, 3] : isMobile ? [5, -5, 4] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
-        reactLogoPosition: isSmall ? [3, 3, 2] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [10, 1, 2],
-        ringPosition: isSmall ? [-8, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-23, 7, 0],
-        targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
-    };
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -6, 0],
+    cubePosition: isSmall
+      ? [4, -5, 3]
+      : isMobile
+      ? [5, -5, 4]
+      : isTablet
+      ? [5, -5, 0]
+      : [9, -5.5, 0],
+    reactLogoPosition: isSmall
+      ? [3, 3, 2]
+      : isMobile
+      ? [5, 4, 0]
+      : isTablet
+      ? [5, 4, 0]
+      : [10, 1, 2],
+    ringPosition: isSmall
+      ? [-8, 7, 0]
+      : isMobile
+      ? [-10, 10, 0]
+      : isTablet
+      ? [-12, 10, 0]
+      : [-23, 7, 0],
+    targetPosition: isSmall
+      ? [-5, -10, -10]
+      : isMobile
+      ? [-9, -10, -10]
+      : isTablet
+      ? [-11, -7, -10]
+      : [-13, -13, -10],
+  };
 };
 
 export const socialLinks = [
-    {
-        id: 1,
-        name: 'LinkedIn',
-        icon: '/assets/linkedin.png',
-        href: 'https://www.linkedin.com/in/igor-montezuma-dev/',
-    },
-    {
-        id: 2,
-        name: 'GitHub',
-        icon: '/assets/github.png',
-        href: 'https://github.com/igor-montezuma-dev',
-    }
+  {
+    id: 1,
+    name: "LinkedIn",
+    icon: "/assets/linkedin.png",
+    href: "https://www.linkedin.com/in/igor-montezuma-dev/",
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    icon: "/assets/github.png",
+    href: "https://github.com/igor-montezuma-dev",
+  },
 ];
 
 export const workExperiences = [
-    {
-        id: 1,
-        name: 'CrosOften',
-        pos: 'Front End Developer',
-        duration: '2024 - Present',
-        title: "Como desenvolvedor Front-End, atuo na implementação de novas features, manutenção e correção de bugs em aplicações de médio e grande porte. Atuo com a metodologia scrum para organização de tarefas e comunicação com a equipe.",
-        icon: '/assets/crosoften_logo.jpg',
-        animation: 'victory',
-    },
-    {
-        id: 2,
-        name: 'Ws Work',
-        pos: 'Full Stack Developer',
-        duration: '2024',
-        title: "Como desenvolvedor Full Stack atuo na implementação de novas features, manutenção e correção de bugs em aplicações de médio e grande porte. Atuo com a metodologia scrum para organização de tarefas e comunicação com a equipe.",
-        icon: '/assets/ws_work_sistemas_logo.jpg',
-        animation: 'clapping',
-    },
-    {
-        id: 3,
-        name: 'Foursys',
-        pos: 'Android Developer',
-        duration: '2022 - 2023',
-        title: "",
-        icon: '/assets/foursys_logo.jpg',
-        animation: 'salute',
-    },
+  {
+    id: 1,
+    name: "CrosOften",
+    pos: "Front End Developer",
+    duration: "2024 - Present",
+    title:
+      "Como desenvolvedor Front-End, atuo na implementação de novas features, manutenção e correção de bugs em aplicações de médio e grande porte. Atuo com a metodologia scrum para organização de tarefas e comunicação com a equipe.",
+    icon: "/assets/crosoften_logo.jpg",
+    animation: "victory",
+  },
+  {
+    id: 2,
+    name: "Ws Work",
+    pos: "Full Stack Developer",
+    duration: "2024",
+    title:
+      "Como desenvolvedor Full Stack atuo na implementação de novas features, manutenção e correção de bugs em aplicações de médio e grande porte. Atuo com a metodologia scrum para organização de tarefas e comunicação com a equipe.",
+    icon: "/assets/ws_work_sistemas_logo.jpg",
+    animation: "clapping",
+  },
+  {
+    id: 3,
+    name: "Foursys",
+    pos: "Android Developer",
+    duration: "2022 - 2023",
+    title: "",
+    icon: "/assets/foursys_logo.jpg",
+    animation: "salute",
+  },
 ];
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,72 +1,70 @@
-import {Canvas} from "@react-three/fiber";
-import {PerspectiveCamera} from "@react-three/drei";
-import HackerRoom from "../components/HackerRoom.jsx";
-import {Suspense} from "react";
-import CanvasLoader from "../components/CanvasLoader.jsx";
-import {useMediaQuery} from "react-responsive";
-import {calculateSizes} from "../constants/index.js";
-import Target from "../components/Target.jsx";
-import ReactLogo from "../components/ReactLogo.jsx";
-import Cube from "../components/Cube.jsx";
-import Rings from "../components/Rings.jsx";
-import HeroCamera from "../components/HeroCamera.jsx";
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
 import Button from "../components/Button.jsx";
-
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import Cube from "../components/Cube.jsx";
+import HackerRoom from "../components/HackerRoom.jsx";
+import HeroCamera from "../components/HeroCamera.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Rings from "../components/Rings.jsx";
+import Target from "../components/Target.jsx";
+import { calculateSizes } from "../constants/index.js";
 
 const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 480 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-    const isSmall = useMediaQuery({maxWidth: 480});
-    const isMobile = useMediaQuery({maxWidth: 768});
-    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
-    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+  return (
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Olá, eu sou o Igor <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Transformando ideias em soluções robustas, e de alto impacto
+        </p>
+      </div>
 
-    return (
-        <section className="min-h-screen w-full flex flex-col relative" id="home">
-            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-                <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-                    Olá, eu sou o Igor <span className="waving-hand">👋</span>
-                </p>
-                <p className="hero_tag text-gray_gradient">Transformando ideias em soluções robustas, e de alto
-                    impacto</p>
-            </div>
-
-            <div className="w-full h-full absolute inset-0">
-                <Canvas className="w-full h-full">
-                    <Suspense fallback={<CanvasLoader/>}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
-                        <HeroCamera isMobile={isMobile}>
-                            <HackerRoom
-                                // scale={0.05}
-                                // position={[0, 0, 0]}
-                                // rotation={[0, -Math.PI / 2, 0]}
-                                position={sizes.deskPosition}
-                                rotation={[0, -Math.PI, 0]}
-                                scale={sizes.deskScale}
-                            />
-                        </HeroCamera>
-                        <group>
-                            <Target position={sizes.targetPosition}/>
-                            <ReactLogo position={sizes.reactLogoPosition}/>
-                            <Rings position={sizes.ringPosition}/>
-                            <Cube position={sizes.cubePosition}/>
-                        </group>
-                        <ambientLight intensity={1}/>
-                        <directionalLight intensity={0.5} position={[10, 10, 10]}/>
-                    </Suspense>
-                </Canvas>
-            </div>
-            <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
-                <a href="#about" className="w-fit">
-                    <Button
-                        name="Que tal trabalharmos juntos?"
-                        isBeam
-                        containerClass="sm:w-fit w-full sm:min-w-96"
-                    />
-                </a>
-            </div>
-        </section>
-
-    )
-}
-export default Hero
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HeroCamera isMobile={isMobile}>
+              <HackerRoom
+                // scale={0.05}
+                // position={[0, 0, 0]}
+                // rotation={[0, -Math.PI / 2, 0]}
+                position={sizes.deskPosition}
+                rotation={[0, -Math.PI, 0]}
+                scale={sizes.deskScale}
+              />
+            </HeroCamera>
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Rings position={sizes.ringPosition} />
+              <Cube position={sizes.cubePosition} />
+            </group>
+            <ambientLight intensity={1} />
+            <directionalLight intensity={0.5} position={[10, 10, 10]} />
+          </Suspense>
+        </Canvas>
+      </div>
+      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+        <a href="#about" className="w-fit">
+          <Button
+            name="Que tal trabalharmos juntos?"
+            isBeam
+            containerClass="sm:w-fit w-full sm:min-w-96"
+          />
+        </a>
+      </div>
+    </section>
+  );
+};
+export default Hero;
